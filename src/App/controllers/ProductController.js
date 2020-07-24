4d233c97c2fedb86b314f12e9d96613dfcfd2bd3
PatiCosta <patricosouza@hotmail.com>
2020-07-24 14:52:51 -0300

    add buy page

--- src/App/controllers/ProductController.js
+++ src/App/controllers/ProductController.js
@@ -1,7 +1,7 @@
 const Category = require('../models/Category')
 const Product = require('../models/Product')
 const File = require('../models/File')
-const { formatPrice } = require('../../lib/utils')
+const { formatPrice, date } = require('../../lib/utils')
 
 
 module.exports = {
@@ -33,6 +33,25 @@ module.exports = {
 
         return res.redirect(`/products/${productId}/edit`)
 
+    },
+    async show (req, res) {
+
+        let results = await Product.find(req.params.id)
+        const product = results.rows[0]
+
+        if(!product) return res.send("Product not found!")
+
+        const {day, hour, minutes, month} = date(product.updated_at)
+
+        product.published = {
+            day:`${day}/${month}`,
+            hour: `${hour}h${minutes}`,
+        }
+
+        product.oldPrice = formatPrice(product.old_price)
+        product.price = formatPrice(product.price)
+
+        return res.render("products/show", {product})
     },
     async edit (req, res) {
         
@@ -92,7 +111,7 @@ module.exports = {
         }
         await Product.update(req.body)
 
-        return res.redirect(`/products/${req.body.id}/edit`)
+        return res.redirect(`/products/${req.body.id}`)
     },
     async delete(req, res) {
         await Product.delete(req.body.id)
